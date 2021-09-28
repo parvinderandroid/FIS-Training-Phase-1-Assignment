321eb2cf2df049b7a6b2c41aa7d7814ed1eb174b
Parvinder Singh Saini <parvinder.android@gmail.com>
2021-09-28 14:20:41 +0530

    Make object name more suitable

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		Application asmt = new Application();
-		asmt.startApplication();
+		Application app = new Application();
+		app.startApplication();
 	}
 }
